b791a59f29350f9a4b67583a798f5c3de59d77aa
Leocrabe225 <petitscrounchs@yahoo.fr>
2023-12-05 22:19:39 +0100

    Chunk class designed, ready to be implemented

--- includes/classes/World/Chunk.hpp
+++ includes/classes/World/Chunk.hpp
@@ -8,23 +8,33 @@
 #include <glm/glm.hpp>
 
 
-# ifndef RLE_HPP
-#  include "RLE.hpp"
-# endif
-
-
 class Chunk
 {
-	
-private :
+protected :
+	int posX;
+	int posY;
 
+	u_char *data;
+	bool isCompiled = false;
+	std::vector<u_char> vertexData;
+	std::vector<u_int> shapeAssemblyData;
 
+	void DataCompiler();
 public :
+	static u_int const sizeX = 16;
+	static u_int const sizeY = 16;
+	static u_int const sizeZ = 256;
 
-	~Chunk();
 	Chunk();
 
-	
+	std::vector<u_char> &GetVertexData();
+	std::vector<u_int> &GetShapeAssemblyData();
+
+	void Generate();
+	void Generate(std::vector<glm::ivec3> positionList, std::vector<glm::ivec3> sizeList);
+	void Generate(u_int seed);
+
+	~Chunk();
 };
 
 #endif
